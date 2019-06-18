703e586fcff43cf36ada0c3dfa648b976d8d844f
mesut@cityuk.email <mesutcityuk.email@mesuts-MacBook-Pro.local>
2019-06-18 10:54:28 +0100

    import country list from JSON file

--- src/components/Countries.vue
+++ src/components/Countries.vue
@@ -1,24 +1,33 @@
 <template>
-    <select @change="changeHandler" v-model="selectedCountry">
+  <select @change="changeHandler" v-model="selectedCountry">
+    <option
+      v-for="country in countryArray"
+      :key="country.iso"
+      :value="country.iso"
+    >{{country.iso}}-{{country.currency}}</option>
+    <!--
       <option value="TR">TR</option>
       <option value="GB">GB</option>
       <option value="FR">FR</option>
       <option value="US">US</option>
-    </select>
+    -->
+  </select>
 </template>
 <script>
+import listCountryJSON from "@/assets/list_country.json";
 export default {
-  props: {
+  props: {    
     propCountry: String
   },
   data: function() {
     return {
+      countryArray: listCountryJSON.country_array,
       selectedCountry: this.propCountry
     };
   },
- 
+
   methods: {
-    changeHandler: function() {     
+    changeHandler: function() {
       this.$emit("countryChangeEvent", this.selectedCountry);
     }
   }
